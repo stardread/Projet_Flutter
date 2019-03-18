e79932f11b1d20c1124ad66b7cc3ee1b66b8cddc
Kevin B <bontemps.kevin1@gmail.com>
2019-03-18 11:28:23 +0100

    lecture et affichage du json
    se fait depuis le github

--- lib/main.dart
+++ lib/main.dart
@@ -1,163 +1,97 @@
-import 'package:auto_size_text/auto_size_text.dart';
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
-import './page/sevices.dart';
-import './page/formulaire.dart';
-import 'package:device_info/device_info.dart';
-import './page/services2.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
-
-void main() => runApp(MaterialApp(
-      title: "Services App",
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
-    ));
-
-class HomePage extends StatefulWidget {
-  @override
-  HomePageState createState() {
-    return new HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  var url =
-      "assets/services.json";
-
-  Services2 services;
-
-  @override
-  void initState() {
-    super.initState();
-
-    fetchData();
-  }
-
-  fetchData() async {
-    var res = await http.get(url);
-    var decodedJson = jsonDecode(url);
-    services = services.fromJson(decodedJson);
-    print(services.toJson());
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Services App"),
-        backgroundColor: Colors.cyan,
-      ),
-      body: services == null
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : GridView.count(
-              crossAxisCount: 2,
-              children: services.services
-                  .map((service) => Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: InkWell(
-                          /*onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => serviceDetail(
-                                          servicemon: service,
-                                        )));
-                          },*/
-                          child: Hero(
-                            tag: service.title,
-                            child: Card(
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  Container(
-                                    height: 100.0,
-                                    width: 100.0,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image: NetworkImage(service.elements.elementAt(0).toString()))),
-                                  ),
-                                  Text(
-                                    service.title,
-                                    style: TextStyle(
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ))
-                  .toList(),
-            ),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.cyan,
-        child: Icon(Icons.refresh),
-      ),
-    );
-  }
-}
-/*
-void main() => runApp(MyApp());
+import './page/formulaire.dart';
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+    return new MaterialApp(
+      home: new MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String url = 'https://raw.githubusercontent.com/stardread/Projet_Flutter/kevin/assets/service.json';
+  List data;
+  Future<String> makeRequest() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var extractdata = json.decode(response.body);
+      data = extractdata["services"];
+    });
+  }
+
+  @override
+  void initState() {
+    this.makeRequest();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [*/
-                /*
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-                *//*
-                Tab(child: AutoSizeText("Services", style : TextStyle(fontSize: 10.0), maxLines: 1)),
-                Tab(child: AutoSizeText("Formulaire", style : TextStyle(fontSize: 10.0), maxLines: 1)),
-                Tab(child: AutoSizeText("Résultats", style : TextStyle(fontSize: 10.0), maxLines: 1)),
-                Tab(child: AutoSizeText("Groupe", style : TextStyle(fontSize: 10.0), maxLines: 1)),
-              ],
-            ),
-            title: Text('Tabs Demo'),
-          ),
-          body: TabBarView(
-            children: [
-              Services(),
-              Formulaire(),
-              Icon(Icons.directions_bike),
-            ],
-          ),
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Services List'),
         ),
-      ),
-    );
+        body: new ListView.builder(
+            itemCount: data == null ? 0 : data.length,
+            itemBuilder: (BuildContext context, i) {
+              return new ListTile(
+                title: new Text(data[i]["title"]),
+                leading: new CircleAvatar(
+                  backgroundImage:
+                      new NetworkImage(data[i]["elements"][0]["value"][0]),
+                ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              //new SecondPage(data[i])
+                              new Formulaire()));
+                },
+              );
+            }
+            )
+            );
   }
 }
 
-*/
+class SecondPage extends StatelessWidget {
+  SecondPage(this.data);
+  final data;
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+      appBar: new AppBar(title: new Text('Second Page')),
+      body: new Center(
+        child: new Container(
+          width: 150.0,
+          height: 150.0,
+          decoration: new BoxDecoration(
+            color: const Color(0xff7c94b6),
+            image: new DecorationImage(
+              image: new NetworkImage(data["picture"]["large"]),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: new BorderRadius.all(new Radius.circular(75.0)),
+            border: new Border.all(
+              color: Colors.red,
+              width: 4.0,
+            ),
+          ),
+        ),
+      ));
+}
