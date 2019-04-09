4056646d92948deb9f4a48dfe17e547285077dae
GOBLET Romain <romain.goblet55@gmail.com>
2019-04-09 10:05:07 +0200

    json to form

--- lib/page/formulaire.dart
+++ lib/page/formulaire.dart
@@ -1,7 +1,14 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import '../ServiceModel.dart';
 import '../Database.dart';
 import 'dart:math' as math;
+//import 'package:json_to_form/json_to_form.dart'; 
+import '../romain/json_to_form_romain.dart';
+
+import 'package:http/http.dart' as http;
 
 
 class Formulaire extends StatefulWidget {
@@ -10,45 +17,52 @@ class Formulaire extends StatefulWidget {
 }
 
 class _Formulaire extends State<Formulaire> {
+  String url = 'https://raw.githubusercontent.com/stardread/Projet_Flutter/kevin/assets/service.json';
+  String data;
+  Future<String> makeRequest() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var extractdata = json.decode(response.body);
+      //for pour rajouter value dans radiobox
+      data = json.encode(extractdata['services'][0]['elements']);
+
+    });
+  }
+  @override
+  void initState() {
+    this.makeRequest();
+  }
+
   var sendersList = ['test1', 'test2', 'test3'];
   var subjectList = ['description1', 'description2', 'description3'];
   final nameController = new TextEditingController();
+  dynamic response;
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Column(
-          children: <Widget>[
-
-            new TextFormField(
-              controller: nameController,
-            ),
-            new TextFormField(
-                controller: nameController,
-            ),
-            new RaisedButton(
-              onPressed: () async{
-
-                Service rnd = new Service(id: 5, title: nameController.text);
-                await DBProvider.db.newService(rnd);
-                setState(() {});
-              },
-            ),
-            new RaisedButton(
-              child: Column(
-                children: <Widget>[
-                  new Text("Destroy")
-                ],
-              ),
-              onPressed: () async{
-
-                await DBProvider.db.destroy();
-                setState(() {});
-              },
-            )
-          ],
-        ),
-      ),
-    );
-  }
+   Widget build(BuildContext context) {
+     return new Scaffold(
+       appBar: new AppBar(
+         title: new Text('Formulaire'),
+       ),
+       body: new SingleChildScrollView(
+         child: new Container(
+           child: new Column(children: <Widget>[
+             new CoreForm(
+               
+               form: data,
+               onChanged: (dynamic response) {
+                 this.response = response;
+               },
+             ),
+             new RaisedButton(
+                 child: new Text('Send'),
+                 onPressed: () {
+                   print(this.response.toString());
+                 })
+           ]),
+         ),
+       ),
+     );
+   }
 }
